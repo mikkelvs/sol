ba65fdcc92ac3e312b49f775acd5615a417670a4
Mikkel Small <mikkelsmall@Mikkels-MacBook-Air.local>
2019-12-22 13:51:16 +0100

    Adding wordpress backend

---
+++ src/components/News.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div>
+
+        <h1>News</h1>
+
+
+
+        <ul>
+            <li v-for="(post, index) in posts" :key="index">{{ post.title.rendered }}</li>
+        </ul>
+
+    </div>
+
+    
+
+</template>
+
+<script>
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
+
+export default {
+    name: 'News',
+    data() {
+        return {
+            posts: []
+        }
+    },
+     mounted() {
+         const vm = this;
+         console.log('LORT', process.env);
+
+         axios.get('wordpress/wp-json/wp/v2/posts').then((response) => {
+             vm.posts = response.data;
+         });
+     }
+}
+
+</script>
